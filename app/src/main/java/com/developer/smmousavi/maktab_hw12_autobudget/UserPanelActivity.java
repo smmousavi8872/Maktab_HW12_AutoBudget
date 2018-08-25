3d349450e6fb8799067f2bc94e0186efb326951c
Mohsen <smmousavi.developer@gmail.com>
2018-08-25 11:26:29 +0430

    Initial commit -Project Created

---
+++ app/src/main/java/com/developer/smmousavi/maktab_hw12_autobudget/UserPanelActivity.java
@@ -0,0 +1,14 @@
+package com.developer.smmousavi.maktab_hw12_autobudget;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserPanelActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_user_panel);
+  }
+}
